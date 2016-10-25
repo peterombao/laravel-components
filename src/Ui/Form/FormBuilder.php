875c9bd30c2dbbaa07dda39570407ec142815b4d
peter.ombao <peter.ombao@republisys.com>
2016-10-25 16:03:54 +0800

    updates

---
+++ src/Ui/Form/FormBuilder.php
@@ -0,0 +1,34 @@
+<?php namespace Peterombao\LaravelComponents\Ui\Form;
+
+
+class FormBuilder {
+
+    protected $sections;
+
+    protected $fields;
+
+    public function setFields($fields)
+    {
+        $this->fields = $fields;
+
+        return $this;
+    }
+
+    public function getFields()
+    {
+        return $this->fields;
+    }
+
+    public function getSections()
+    {
+        return $this->sections;
+    }
+
+    public function setSections($sections)
+    {
+        $this->sections = $sections;
+
+        return $this;
+    }
+
+} 
